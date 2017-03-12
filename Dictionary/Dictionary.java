33a371242d898f42ac414ca6eab974c5d749c83c
Ashwin <ashwin.kamalakannan@uoit.net>
2017-03-12 18:26:32 -0400

    added hash algorithm and other methods for dictionary class

--- Dictionary/Dictionary.java
+++ Dictionary/Dictionary.java
@@ -1,80 +1,35 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
+package dictionary;
 
-class Dictionary {
-
-    private int size;
-    List<Entry> entries;
-
-    // creates empty dictionary
-    public Dictionary() {
-        entries = new ArrayList<Entry>();
-    }
-
-    // creates dictionary with entries from specified file
-    public Dictionary(String fname) {
-        entries = importFromFile(fname);
-        // TODO: load entries from key-value pairs in file
-    }
-
-    // returns the specified entry and the corresponding value
-    public String check(String key) {
-        String str = "";
-        for (Entry i : entries) {
-            if (i.getKey().equals(key))
-                str = key + " - " + i.getValue();
-        }
-        if (str.equals(""))
-            return " <key not found> ";
-        return str;
-    }
-
-    // returns the whole dictionary as a table
-    public String read() {
-        String str = "\r\nword - definition\r\n=================\r\n";
-        for (Entry i : entries)
-            str += i.getKey() + " - " + i.getValue() + "\r\n";
-        return str;
-    }
-
-    // checks if key is already in dictionary, if not then adds as new entry
-    public void addEntry(String key, String value) {
-        for (Entry i : entries) {
-            if (i.getKey().equals(key)) {
-                System.out.println(" <key already exists> s");
-                return;
-            }
-        }
-        entries.add(new Entry(key, value));
-    }
-
-    public void removeEntry(String key) {
-        // TODO: remove entry from dictionary based on key given
-    }
-
-    public void editEntry(String key) {
-        // TODO: edit key and value for specifed key
-    }
-
-    // load dictionary data from a text file
-    // first line should be key, second line should be value, and so on..
-    public ArrayList<Entry> importFromFile(String fname) {
-        ArrayList<Entry> buffer = new ArrayList<Entry>();
-        try {
-            File inFile = new File(fname);
-            Scanner input = new Scanner(inFile);
-            while(input.hasNext())
-                buffer.add(new Entry(input.nextLine().toLowerCase(), input.nextLine().toLowerCase()));
-            input.close();
-        } catch (FileNotFoundException e) {
-            System.out.println(" <file not found>");
-        }
-        return buffer;
-    }
-
-    // write dictionary data to a text file
-    public void exportToFile() {
-        // TODO: export entries to file
-    }
+public class Dictionary {
+	
+	Entry[] entries; // Array to store the entries
+	
+	public Dictionary(String fName) {
+		entries = new Entry[200000];
+		System.out.println(entries.length);
+		System.out.println("index of apple " + convertHash("apple"));
+	}
+	
+	public boolean add(String key, String value) {
+		// TODO check if key already exists, if not add new entry
+		return false; // return true if successfully added a new entry
+	}
+	
+	public String search(String key) {
+		// TODO return value of corresponding key from dictionary
+		return null;
+	}
+	
+	public boolean update(String)
+	
+	public void Load(int size) {
+		// TODO load entries[] from file
+	}
+	
+	public int convertHash(String s) { // tested with up to 25 characters
+		int hash = 7;
+		for (int i=0; i<s.length(); i++)
+			hash = (hash*31 + s.charAt(i)) % entries.length;
+		return hash;
+	}
 }
--- Dictionary/Entry.java
+++ Dictionary/Entry.java
@@ -1,37 +1,11 @@
+package dictionary;
 
-class Entry {
-
-    private String key, value;
-
-    // initialzed entry
-    public Entry(String key, String value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    // empty entry
-    public Entry() {
-        key = null;
-        value = null;
-    }
-
-    // returns value of 'key'
-    public String getKey() {
-        return key;
-    }
-
-    // return value of 'value'
-    public String getValue() {
-        return value;
-    }
-
-    // set value of 'key'
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    // set value of 'value'
-    public void setValue(String value) {
-        this.value = value;
-    }
+public class Entry {
+	
+	private String key, value;
+	
+	public Entry(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
 }
