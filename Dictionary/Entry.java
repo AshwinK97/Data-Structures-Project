e9fae95576eade9790a62db6c6899c9809430c61
Ashwin Kamalakannan <ashwin.kamalakannan@uoit.net>
2017-03-13 13:10:20 -0400

    added hash to entry class and get/set methods

--- Dictionary/Entry.java
+++ Dictionary/Entry.java
@@ -3,9 +3,27 @@ package dictionary;
 public class Entry {
 	
 	private String key, value;
+	private int hash;
 	
-	public Entry(String key, String value) {
+	public Entry(String key, String value, int hash) {
 		this.key = key;
 		this.value = value;
+		this.hash = hash;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public int getHash() {
+		return hash;
 	}
 }
