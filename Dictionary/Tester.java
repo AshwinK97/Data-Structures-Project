3602c3ff24bb2ad43291934ad10d2961b2f92ce3
Ashwin Kamalakannan <ashwin.kamalakannan@uoit.net>
2017-03-13 13:10:58 -0400

    update test cases to match exersices

--- Dictionary/Tester.java
+++ Dictionary/Tester.java
@@ -1,8 +1,16 @@
 package dictionary;
 
+import java.io.FileNotFoundException;
+
 public class Tester {
 	
-	public static void main(String args[]) {
-		Dictionary book = new Dictionary("dictionary.csv");
+	public static void main(String args[]) throws FileNotFoundException {
+		Dictionary book100 = new Dictionary(100);
+		System.out.println(book100.toString());
+//		Dictionary book1000 = new Dictionary(1000);
+//		Dictionary book2500 = new Dictionary(2500);
+//		Dictionary book5000 = new Dictionary(5000);
+//		Dictionary book10000 = new Dictionary(10000);
+//		Dictionary bookAll = new Dictionary(-1);
 	}
 }
