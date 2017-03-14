294f1ff1c768f0479c819aefa8d305cdd8613e3d
Ashwin Kamalakannan <ashwin.kamalakannan@uoit.net>
2017-03-13 21:16:35 -0400

    updated test cases to print dictionaries

--- Dictionary/Tester.java
+++ Dictionary/Tester.java
@@ -8,9 +8,14 @@ public class Tester {
 		Dictionary book100 = new Dictionary(100);
 		System.out.println(book100.toString());
 //		Dictionary book1000 = new Dictionary(1000);
+//		System.out.println(book1000.toString());
 //		Dictionary book2500 = new Dictionary(2500);
+//		System.out.println(book2500.toString());
 //		Dictionary book5000 = new Dictionary(5000);
+//		System.out.println(book5000.toString());
 //		Dictionary book10000 = new Dictionary(10000);
+//		System.out.println(book10000.toString());
 //		Dictionary bookAll = new Dictionary(-1);
+//		System.out.println(bookAll.toString());
 	}
 }
